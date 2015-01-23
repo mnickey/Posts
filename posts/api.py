f22cd7879fb1ff7edf3b49f51bd60e16a8b18482
Michael Nickey <mnickey@gmail.com>
2015-01-22 19:54:25 -0800

    Added PUT method for updating posts

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,15 @@ import decorators
 from posts import app
 from database import session
 
+# JSON Schema describing the structure of a post
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -34,20 +43,68 @@ def posts_get():
     data = json.dumps([post.as_dictionary() for post in posts])
     return Response(data, 200, mimetype="application/json")
 
-@app.route("/api/posts/<int:id>", methods=["GET"])
+@app.route("/api/posts/<int:id>", methods=["GET", "PUT"])
 @decorators.accept("application/json")
 def post_get(id):
     """ Single post endpoint """
     # Get the post from the database
-    post = session.query(models.Post).get(id)
 
-    # Check whether the post exists
-    # If not return a 404 with a helpful message
-    if not post:
-        message = "Could not find post with id {}".format(id)
+    # create a conditional for GET and PUT
+    if request.method == "GET":
+        post = session.query(models.Post).get(id)
+
+        # Check whether the post exists
+        # If not return a 404 with a helpful message
+        if not post:
+            message = "Could not find post with id {}".format(id)
+            data = json.dumps({"message": message})
+            return Response(data, 404, mimetype="application/json")
+    elif request.method == "PUT":
+        # assigning the data to the variable 'post'
+        post = session.query(models.Post).get(id)
+        # printing the data to debug using repr. This will print the data in a string
+        # print "Request.data is: ", repr(request.data)
+        # Since the data is coming in as a json, use json.loads to create a python dictionary with it
+        # Assign the data in dictionary form, to a variable to use. In this case req_d
+        req_d = json.loads(request.data)
+        # Assign the post.title and post.body from the new info passed in
+        post.title = req_d["title"]
+        post.body =  req_d["body"]
+        # commit the changes since the data is "in transit" at this time.
+        session.commit()
+    else:
+        message = "This is not a valid HTTP method"
         data = json.dumps({"message": message})
-        return Response(data, 404, mimetype="application/json")
+        return Response(data, 500, mimetype="application/json")
 
     # Return the post as JSON
     data = json.dumps(post.as_dictionary())
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not we return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
+
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -81,24 +81,17 @@ class TestAPI(unittest.TestCase):
         """ Getting a single post which doesn't exist """
         response = self.client.get("/api/posts/1",
                     headers=[("Accept", "application/json")])
-
-        self.assertEqual(response.status_code, 404) #Changd from given 404 to 406 to make test pass
+        self.assertEqual(response.status_code, 404)
         self.assertEqual(response.mimetype, "application/json")
 
         data = json.loads(response.data)
         self.assertEqual(data["message"], "Could not find post with id 1")
-        # Request must accept application/json data message changed from
-        # Given: Could not find post with id 1
-
     def testUnsupportedAcceptHeaders(self):
         response = self.client.get("/api/posts",
                    headers=[("Accept", "application/xml")] )
-        # Why is this getting a respose code of 200???
-        # When changing this to 200 from 406, this passes...
         self.assertEqual(response.status_code, 406)
         self.assertEqual(response.mimetype, "application/json")
         data = json.loads(response.data)
-        # until it gets here...
         self.assertEqual(data["message"],
                          "Request must accept application/json data")
     def testGetPostsWithTitle(self):
@@ -154,6 +147,93 @@ class TestAPI(unittest.TestCase):
         post = posts[1]
         self.assertEqual(post["title"], "Post with green eggs and ham")
         self.assertEqual(post["body"], "Another eggs test")
+
+    def testPostPost(self):
+        """ Posting a new post """
+        data = {
+            "title": "Example Post",
+            "body": "Just a test"
+        }
+
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/posts/1")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["title"], "Example Post")
+        self.assertEqual(data["body"], "Just a test")
+
+        posts = session.query(models.Post).all()
+        self.assertEqual(len(posts), 1)
+
+        post = posts[0]
+        self.assertEqual(post.title, "Example Post")
+        self.assertEqual(post.body, "Just a test")
+    def testUnsupportedMimetype(self):
+        data = "<xml></xml>"
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/xml",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 415)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(data["message"],
+                         "Request must contain application/json data")
+    def testInvalidData(self):
+        """ Posting a post with an invalid body. """
+        data = {
+            "title": "Example Post",
+            "body": 32 }
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")] )
+        self.assertEqual(response.status_code, 422)
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "32 is not of type 'string'")
+    def testMissingData(self):
+        """ Posting a post with a missing body. """
+        data = {
+            "title": "Example Post" }
+        response = self.client.post("/api/posts",
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=[("Accept", "application/json")] )
+        self.assertEqual(response.status_code, 422)
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "'body' is a required property" )
+    def testPutPost(self):
+        """ Putting a single post from a populated database """
+        postA = models.Post(title="Example Post A", body="Just a test")
+        postB = models.Post(title="Example Post B", body="Still just a test")
+
+        session.add_all([postA, postB])
+        session.commit()
+
+        newPost= {"title":"Example Post B", "body":"Still just a tester"}
+
+        response = self.client.put("/api/posts/{}".format(postB.id),
+                    data = json.dumps(newPost),
+                    headers = [("Accept", "application/json")] )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        post = json.loads(response.data)
+        self.assertEqual(post["title"], "Example Post B")
+        self.assertEqual(post["body"], "Still just a tester")
 
 if __name__ == "__main__":
     unittest.main()
